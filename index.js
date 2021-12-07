62f3c1052bfa3ea626b5ea1e388b2688372da0a9
peterejiro <peterejiro96@gmail.com>
2021-12-07 19:55:24 +0100

    jwt implemented

--- index.js
+++ index.js
@@ -3,38 +3,31 @@
     const bodyParser = require('body-parser');
     const cors = require('cors');
    // const db = require("./services/db")
-
-
-
     const app = express();
-
     app.use(cors());
     app.use(express.json());
     dotenv.config();
 
     const employeeRouter = require('./routes/employees')
-    const departRouter = require('./routes/department');
+    app.use('/employees', employeeRouter);
 
-   app.use('/employees', employeeRouter);
+    const userRouter = require('./routes/users')
+    app.use('/users', userRouter);
 
 
     /* Error handler middleware */
     app.use((err, req, res, next) => {
         const statusCode = err.statusCode || 500;
         console.error(err.message, err.stack);
-        res.status(statusCode).json({'message': err.message});
-        return;
+        return res.status(statusCode).json({'message': err.message});
+
     });
 
 
-    app.get('/', (req, res)=>{
 
-        res.send('Hello World')
-    });
 
-    app.get('/api/courses', (req, res)=>{
-        res.send([1, 2, 3]);
-    })
+
+
     const port = process.env.PORT || 9500
 
     app.listen(port, ()=>{
---
+++ models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true // Automatically gets converted to SERIAL for postgres
+    },
+    user_username:  DataTypes.STRING,
+    user_name:  DataTypes.STRING,
+    user_email: DataTypes.STRING,
+    user_password: DataTypes.STRING,
+    user_type: DataTypes.INTEGER,
+    user_token: DataTypes.STRING,
+    user_status: DataTypes.INTEGER,
+
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
