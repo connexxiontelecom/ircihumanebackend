6787f40c0c770b802a64605b4b7e9ebb5c49a14c
peterejiro <peterejiro96@gmail.com>
2021-12-13 13:47:57 +0100

    Cleaned up login code, and services module to remove unused dependencies, while implementing location allowance feature

---
+++ models/locationallowance.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class locationAllowance extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  locationAllowance.init({
+    la_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true // Automatically gets converted to SERIAL for postgres
+    },
+    la_payment_id: DataTypes.NUMBER,
+    la_location_id: DataTypes.NUMBER,
+    la_amount: DataTypes.DECIMAL
+  }, {
+    sequelize,
+    modelName: 'locationAllowance',
+    tableName: 'location_allowance'
+  });
+  return locationAllowance;
+};
--- routes/pension-provider.js
+++ routes/pension-provider.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const {parse, stringify, toJSON, fromJSON} = require('flatted');
-
 const pensionprovider = require('../services/pensionProivderService');
 
 /* Pension provider routes. */
