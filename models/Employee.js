1704ae501c984e743848cc11bf07c17327b1e12f
peterejiro <peterejiro96@gmail.com>
2022-01-04 06:45:14 +0100

    function to set employee to supervisor status, and also assign supervisor id

--- models/Employee.js
+++ models/Employee.js
@@ -62,6 +62,8 @@ module.exports = (sequelize, DataTypes) => {
         emp_salary_structure_setup:DataTypes.INTEGER,
         emp_salary_structure_category:DataTypes.INTEGER,
         emp_tax_amount:DataTypes.DOUBLE,
+        emp_supervisor_status:DataTypes.INTEGER,
+        emp_supervisor_id:DataTypes.INTEGER,
         createdAt: {
             field: 'created_at',
             type: DataTypes.DATE,
