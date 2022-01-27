ee892b5864f6ef1ac99d301b25c561b055faea4c
peterejiro <peterejiro96@gmail.com>
2022-01-27 12:28:09 +0100

    edit: update pd based on basic or total percentage gross - almost complete

--- routes/paymentDefinitions.js
+++ routes/paymentDefinitions.js
@@ -33,7 +33,10 @@ router.post('/add-payment-definition', auth,  async function(req, res, next) {
             pd_desc: Joi.alternatives().try(Joi.string(), Joi.number()),
             pd_basic: Joi.number().required(),
             pd_tie_number: Joi.alternatives().try(Joi.string(), Joi.number()),
-            pd_pr_gross: Joi.number().precision(2)
+            pd_pr_gross: Joi.number().precision(2),
+            pd_value:Joi.number(),
+            pd_amount: Joi.number(),
+            pd_percentage: Joi.number().precision(2)
         })
 
         const paymentDefinitionRequest = req.body
@@ -137,11 +140,15 @@ router.patch('/update-payment-definition/:pd_id', auth,  async function(req, res
             pd_payment_type: Joi.number().required(),
             pd_payment_variant: Joi.number().required(),
             pd_payment_taxable: Joi.number().required(),
-            pd_desc: Joi.number(),
+            pd_desc: Joi.alternatives().try(Joi.string(), Joi.number()),
             pd_basic: Joi.number().required(),
-            pd_tie_number: Joi.string()
+            pd_tie_number: Joi.alternatives().try(Joi.string(), Joi.number()),
+            pd_pr_gross: Joi.number().precision(2),
+            pd_value:Joi.number(),
+            pd_amount: Joi.number(),
+            pd_percentage: Joi.number().precision(2)
         })
-
+        let updateResponse
         const paymentDefinitionRequest = req.body
 
         const validationResult = schema.validate(paymentDefinitionRequest)
@@ -149,41 +156,51 @@ router.patch('/update-payment-definition/:pd_id', auth,  async function(req, res
         if(validationResult.error){
             return res.status(400).json(validationResult.error.details[0].message)
         }
-        await paymentDefinition.findPaymentById(req.params['pd_id']).then((data) =>{
-            if(data){
+        const paymentDefinitionDetails = await paymentDefinition.findPaymentById(req.params['pd_id']).then((data) =>{
+        return data
+        })
 
-
-                paymentDefinition.findPaymentByCode(paymentDefinitionRequest.pd_payment_code).then((data)=>{
-                    if(data){
-                        if(data.pd_id === parseInt(req.params['pd_id'])){
-                            paymentDefinition.updatePaymentDefinition(paymentDefinitionRequest, req.params['pd_id']).then((data)=>{
-                                const logData = {
-                                    "log_user_id": req.user.username.user_id,
-                                    "log_description": "Updated payment definition",
-                                    "log_date": new Date()
-                                }
-                                logs.addLog(logData).then((logRes)=>{
-                                    //return res.status(200).json(logRes);
-                                    return  res.status(200).json(`Payment Definition Updated`)
-                                })
-
-                            })
-                        }else{
-                            return res.status(400).json('payment code already exist')
-                        }
-                    }else{
-                        paymentDefinition.updatePaymentDefinition(paymentDefinitionRequest, req.params['pd_id']).then((data)=>{
-                            return res.status(200).json(`Payment Definition Updated`)
-                        })
-                    }
-                })
-
-
+            if(_.isNull(paymentDefinitionDetails) || _.isEmpty(paymentDefinitionDetails)){
+                         return res.status(404).json(`Payment Definition doesn't exist`)
 
             }else{
-                return res.status(404).json(`Payment Definition doesn't exist`)
+                    if(parseInt(paymentDefinitionRequest.pd_basic) === 1){
+
+                        const basicSalary = await paymentDefinition.findBasicPaymentDefinition().then((data)=>{
+                            return data
+                        })
+
+                        if(_.isNull(basicSalary) || _.isEmpty(basicSalary)){
+
+                            updateResponse = await updatePaymentDefinition(paymentDefinitionRequest, req.params['pd_id'])
+
+                        }
+                        else{
+                            if(basicSalary.pd_id === req.params['pd_id']){
+
+                                updateResponse = await updatePaymentDefinition(paymentDefinitionRequest, req.params['pd_id'])
+
+                            }
+                            else{
+                                return res.status(400).json(`Basic Salary Already Exist`)
+
+                            }
+
+                        }
+
+
+                    }else{
+
+                        updateResponse = await updatePaymentDefinition(paymentDefinitionRequest, req.params['pd_id'])
+
+                    }
+
+
+
+
+
             }
-        })
+
     } catch (err) {
 
         console.error(`Error while updating payment definitions `, err.message);
@@ -191,5 +208,36 @@ router.patch('/update-payment-definition/:pd_id', auth,  async function(req, res
     }
 });
 
+
+async function updatePaymentDefinition(paymentDefinitionRequest, paymentDefinitionId){
+    const paymentDefinitionByCode = await  paymentDefinition.findPaymentByCode(paymentDefinitionRequest.pd_payment_code).then((data)=>{
+        return data
+    })
+    if(_.isEmpty(paymentDefinitionByCode) || _.isNull(paymentDefinitionByCode)){
+        if(paymentDefinitionByCode.pd_id === parseInt(paymentDefinitionId)){
+
+            await paymentDefinition.updatePaymentDefinition(paymentDefinitionRequest, paymentDefinitionId).then((data)=>{
+                const logData = {
+                    "log_user_id": req.user.username.user_id,
+                    "log_description": "Updated payment definition",
+                    "log_date": new Date()
+                }
+                logs.addLog(logData).then((logRes)=>{
+                            return  data
+                })
+
+            })
+        }
+        else{
+            return 'error'
+        }
+    }
+    else{
+        await paymentDefinition.updatePaymentDefinition(paymentDefinitionRequest, paymentDefinitionId).then((data)=>{
+            return data
+        })
+    }
+
+}
 
 module.exports = router;
