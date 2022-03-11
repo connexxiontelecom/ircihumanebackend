f18e51f799c8a0df54297a1b031d2fcf9eb33635
ejirogheneoki-peter <peterejiro96@gmail.com>
2022-03-11 06:08:13 +0100

    employee documents model

---
+++ models/employeedocuments.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class EmployeeDocuments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  EmployeeDocuments.init({
+    ed_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true // Automatically gets converted to SERIAL for postgres
+    },
+    ed_empid: DataTypes.INTEGER,
+    ed_docs: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'EmployeeDocuments',
+  });
+  return EmployeeDocuments;
+};
