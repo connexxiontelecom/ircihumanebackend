12821a109212b4586aeb76f9888e8ff7735e2039
peterejiro <peterejiro96@gmail.com>
2022-04-08 11:45:37 +0100

    nhf collumn on pd table

---
+++ migrations/20220408103533-paymentDefinitionNhf.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn(
+          'payment_definitions',
+          'pd_nhf',
+          {
+            type: Sequelize.INTEGER,
+            allowNull:true,
+            comment:'1 is true 0 is false',
+            defaultValue: 0
+          }
+      ),
+
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
