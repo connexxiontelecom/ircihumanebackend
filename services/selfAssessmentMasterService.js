615077f80de403eefa9bff410bb9bc198daded2d
peterejiro <peterejiro96@gmail.com>
2022-04-15 20:34:00 +0100

    self assessment table update

---
+++ services/selfAssessmentMasterService.js
@@ -0,0 +1,23 @@
+const {QueryTypes} = require('sequelize')
+const {sequelize, Sequelize} = require('./db');
+const SelfAssessmentMaster = require("../models/selfassessment")(sequelize, Sequelize.DataTypes)
+
+
+async function addSelfAssessmentMaster(selfAssessmentData) {
+    return await SelfAssessmentMaster.create({
+        sam_gs_id: selfAssessmentData.sam_gs_id,
+        sam_emp_id: selfAssessmentData.sam_emp_id,
+        sam_status: selfAssessmentData.sam_status,
+        sam_supervisor_id: selfAssessmentData.sam_supervisor_id
+    });
+}
+
+async function removeSelfAssessmentMaster(gsId, empId) {
+    return await SelfAssessmentMaster.destroy({where: {sam_gs_id: gsId, sam_emp_id: empId}})
+}
+
+
+module.exports = {
+    addSelfAssessmentMaster,
+    removeSelfAssessmentMaster,
+}
