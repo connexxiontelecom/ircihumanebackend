184d5e06ef17fa06f32dad19c298e1d154dada9d
peterejiro <peterejiro96@gmail.com>
2022-05-24 15:43:16 +0100

    refactored auth middleware to arrow functions

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,32 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = auth
+//module.exports = auth
+
+module.exports = ( roles = [])=>{
+    try{
+        if (typeof roles === 'string') {
+            roles = [roles];
+        }
+        return [
+            (req, res, next) => {
+                const token = req.header("x-auth-token");
+                if (!token) return res.status(403).json("Access denied.");
+                req.user = jwt.verify(token, process.env.TOKEN_SECRET);
+                let userRoles = req.user.username.permission
+                if (roles.length && !containsAny(userRoles,roles)) {
+                    return res.status(401).json('Unauthorised');
+                }
+                next();
+            }
+        ]
+    }catch(error){
+        return [
+            (req, res, next) => {
+                res.status(403).json("Invalid token");
+            }
+        ]
+    }
+}
 
 
 //     (req, res, next) => {
@@ -16,33 +42,33 @@ module.exports = auth
 // };
 
 
-function auth(roles = []) {
-    if (typeof roles === 'string') {
-        roles = [roles];
-    }
-    try {
-        return [
-            (req, res, next) => {
-                const token = req.header("x-auth-token");
-                if (!token) return res.status(403).json("Access denied.");
-                req.user = jwt.verify(token, process.env.TOKEN_SECRET);
-                let userRoles = req.user.username.permission
-                if (roles.length && !containsAny(userRoles,roles)) {
-                    return res.status(401).json('Unauthorised');
-                }
-                next();
-            }
-        ]
-
-    } catch (error) {
-        return [
-            (req, res, next) => {
-                res.status(403).send("Invalid token");
-            }
-        ]
-    }
-
-}
+// function auth(roles = []) {
+//     if (typeof roles === 'string') {
+//         roles = [roles];
+//     }
+//     try {
+//         return [
+//             (req, res, next) => {
+//                 const token = req.header("x-auth-token");
+//                 if (!token) return res.status(403).json("Access denied.");
+//                 req.user = jwt.verify(token, process.env.TOKEN_SECRET);
+//                 let userRoles = req.user.username.permission
+//                 if (roles.length && !containsAny(userRoles,roles)) {
+//                     return res.status(401).json('Unauthorised');
+//                 }
+//                 next();
+//             }
+//         ]
+//
+//     } catch (error) {
+//         return [
+//             (req, res, next) => {
+//                 res.status(403).send("Invalid token");
+//             }
+//         ]
+//     }
+//
+// }
 
 function containsAny(source,target)
 {
