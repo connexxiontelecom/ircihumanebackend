e5b0de5cde035733c5c153e62b729b948d164fd7
peterejiro <peterejiro96@gmail.com>
2022-05-25 11:33:06 +0100

    salary mapping detail model and migration

--- migrations/20220525095710-create-salarymappingmaster.js
+++ migrations/20220525095710-create-salarymappingmaster.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('salarymappingmasters', {
+    await queryInterface.createTable('salary_mapping_master', {
       smm_id: {
         allowNull: false,
         autoIncrement: true,
---
+++ migrations/20220525101437-create-salarymappingdetails.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('salary_mapping_details', {
+      smd_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      smd_master_id: {
+        type: Sequelize.INTEGER
+      },
+      smd_ref_code: {
+        type: Sequelize.STRING
+      },
+      smd_location_t7: {
+        type: Sequelize.STRING
+      },
+
+      smd_donor_t1:{
+        type: Sequelize.STRING
+      },
+
+      smd_salary_expense_t2s:{
+        type: Sequelize.STRING
+      },
+
+      smd_benefit_expense_t2b:{
+        type: Sequelize.STRING
+      },
+
+      smd_allocation:{
+        type: Sequelize.DOUBLE
+      },
+
+      smd_status:{
+        type: Sequelize.INTEGER
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('salary_mapping_details');
+  }
+};
