7e9e0af6b00be004d5bdc0d396befd61838fd753
talktojoegee <talktojoegee@gmail.com>
2022-08-01 15:27:57 +0100

    Leave accrual commit

--- routes/leaveAccrual.js
+++ routes/leaveAccrual.js
@@ -158,7 +158,7 @@ router.post('/year', async (req, res)=>{
 
     return res.status(200).json(data);
   }catch (e) {
-    return res.status(400).json("Something went wrong.");
+    return res.status(400).json("Something went wrong."+e.message);
   }
 });
 
