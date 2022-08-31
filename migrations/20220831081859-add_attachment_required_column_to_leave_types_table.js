d0e8ebbb3713beb149c3bcb8f4091d9a40b895e2
talktojoegee <talktojoegee@gmail.com>
2022-08-31 16:35:07 +0100

    Added a new column to leave types; called attachment_required. Takes either 1 or 0

---
+++ migrations/20220831081859-add_attachment_required_column_to_leave_types_table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn(
+        'leave_types',
+        'lt_attachment_required',
+        {
+          type: Sequelize.INTEGER,
+          allowNull:true,
+          defaultValue:0,
+          comment:'0=No,1=Yes'
+        }
+      ),
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
