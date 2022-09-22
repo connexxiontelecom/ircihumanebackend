603183fab60324281e8917ed886b4137b94c148a
talktojoegee <talktojoegee@gmail.com>
2022-09-22 16:45:57 +0100

    Accrual validation for annual and sick leave ignored

--- routes/leaveApplication.js
+++ routes/leaveApplication.js
@@ -164,15 +164,17 @@ router.post('/add-leave-application', auth(), async function (req, res, next) {
                 return data
             });
 
+            if(parseInt(req.body.leapp_leave_type) !== 1 || parseInt(req.body.req.body.leapp_leave_type) !== 2){
+                if (_.isNull(accruedDays) || accruedDays === 0) {
+                  return res.status(400).json('No Leave Accrued for Selected Leave')
+                }
 
-            if (_.isNull(accruedDays) || accruedDays === 0) {
-                return res.status(400).json('No Leave Accrued for Selected Leave')
+
+                if (parseInt(daysRequested) > parseInt(accruedDays)) {
+                  return res.status(400).json("Days Requested Greater than Accrued Days")
+                }
             }
 
-
-            if (parseInt(daysRequested) > parseInt(accruedDays)) {
-                return res.status(400).json("Days Requested Greater than Accrued Days")
-            }
         }
 
         leaveApplicationRequest['leapp_year'] = startYear
