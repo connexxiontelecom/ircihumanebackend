49744c29e6fbabbb9875a19440e85d4d9369d62a
peterejiro <peterejiro96@gmail.com>
2022-10-05 18:40:35 +0200

    fix unknown collum error

--- services/salaryService.js
+++ services/salaryService.js
@@ -149,7 +149,7 @@ async function getEmployeeSalaryByUniqueIdAndMonthYear(uniqueId, month, year, pd
 async function getEmployeeSalaryByD7AndMonthYear(d7, month, year, pd) {
     return await Salary.findOne({
         where: {
-            salary_emp_d7: d7,
+            salary_d7: d7,
             salary_paymonth: month,
             salary_payyear: year,
             salary_pd: pd
