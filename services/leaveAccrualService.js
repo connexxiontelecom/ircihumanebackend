4324f7d81f5215c0aa0df904d6dd9b5ab863c2d5
peterejiro <peterejiro96@gmail.com>
2022-10-11 23:41:44 +0200

    modify  cronjob to run daily

--- services/leaveAccrualService.js
+++ services/leaveAccrualService.js
@@ -78,6 +78,18 @@ async function findLeaveAccrualByLeaveApplication(empId, month, year, type) {
     })
 }
 
+async function findLeaveAccrualByLeaveTypePositive(empId, month, year, type) {
+    return await LeaveAccrual.findOne({
+      where: {
+          lea_emp_id: empId,
+          lea_month: month,
+          lea_year: year,
+          lea_leave_type: type,
+          lea_rate: {[Op.gt]: 0}
+      }
+    })
+}
+
 
 async function findLeaveAccrualByYearEmployeeLeaveType(year, employee_id, leave_type) {
     return await LeaveAccrual.findAll({where: {lea_emp_id: employee_id, lea_year: year, lea_leave_type: leave_type}})
@@ -184,6 +196,7 @@ module.exports = {
   archiveLeaveAccrualByLeaveApplication,
   findLeaveAccrualByLeaveApplication,
   getLeaveAccruals,
-  archiveAccrual
+  archiveAccrual,
+    findLeaveAccrualByLeaveTypePositive
 
 }
