913b1d1469e69cd73d2fd931bbc1cf49558ca71c
talktojoegee <talktojoegee@gmail.com>
2023-12-20 09:53:40 +0100

    Changed db config to default

--- services/db.js
+++ services/db.js
@@ -2,17 +2,19 @@ const { Sequelize, QueryTypes } = require('sequelize');
 const dotenv = require('dotenv');
 
 dotenv.config();
-/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
-});*/
+});
 
+/*
 const sequelize = new Sequelize('ircihumane', 'root', 'root', {
     host: 'localhost',
     port: 8889,
     dialect: 'mysql'
 });
+*/
 
 try {
     sequelize.authenticate().then(r =>  console.log('Connection has been established successfully.') );
